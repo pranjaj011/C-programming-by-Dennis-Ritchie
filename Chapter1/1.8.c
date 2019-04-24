8be7f745bcab0dd16741d278b680aaa63a9e778a
pranjaj011 <49923043+pranjaj011@users.noreply.github.com>
2019-04-24 11:36:05 -0700

    Create 1.8.c

---
+++ Chapter1/1.8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  int blanks, tabs, newlines;
+  int c;
+  int done = 0;
+  int lastchar = 0;
+
+  blanks = 0;
+  tabs = 0;
+  newlines = 0;
+
+  while(done == 0)
+  {
+    c = getchar();
+
+    if(c == ' ')
+      ++blanks;
+
+    if(c == '\t')
+      ++tabs;
+
+    if(c == '\n')
+      ++newlines;
+
+    if(c == EOF)
+    {
+      if(lastchar != '\n')
+      {
+        ++newlines;
+                     
+      }
+      done = 1;
+    }
+    lastchar = c;
+  }
+
+  printf("Blanks: %d\nTabs: %d\nLines: %d\n", blanks, tabs, newlines);
+}
