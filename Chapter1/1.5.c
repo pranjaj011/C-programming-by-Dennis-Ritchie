ee992862851e722eee72045109788560959a77d6
pranjaj011 <49923043+pranjaj011@users.noreply.github.com>
2019-04-24 09:11:32 -0700

    Update 1.5.c

--- Chapter1/1.5.c
+++ Chapter1/1.5.c
@@ -4,9 +4,10 @@
 #define UPPER	300	// upper limit of table
 #define STEP	20	// step size
 // fahrenheit-celsius table
-int main()
+main()
 {
-	int fahr;
+	printf("Fahrenheit-Celsius\n");
+    int fahr;
 	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
 		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
